f89e7135d3e953e5ad83efaa7b420626787e99c1
hardworkingmission <shakil20131101041@gmail.com>
2022-06-26 17:58:13 +0600

    update-1.2

--- db/connectBD.js
+++ db/connectBD.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = () => {
-  return mongoose.connect(process.env.URI);
+  return mongoose.connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 module.exports = connectDB;
